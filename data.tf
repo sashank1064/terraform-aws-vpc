db9a03e447aebb8c84a87aa9240ce8cda939f9e1
sashank <sashanksai1064@gmail.com>
2026-01-04 22:04:40 -0800

    terraform vpc module

--- data.tf
+++ data.tf
@@ -3,6 +3,19 @@ data "aws_availability_zones" "available" {
 
 }
 
+data "aws_vpc" "default" {
+  default = true
+  
+}
+
+data "aws_route_table" "main" {    # gets the main route table of default VPC
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name = "association.main"
+    values = ["true"]
+  }
+}
+
 /* output "azs_info" {
     value = data.aws_availability_zones.available
   
--- variables.tf
+++ variables.tf
@@ -16,9 +16,108 @@ variable "Project" {
     type        = string
    
  }
+
+ variable "vpc_tags" {
+    description = "A map of tags to assign to the VPC"
+    type        = map(string)
+    default     = {}
+  
+}
+
+variable "igw_tags" {
+    description = "A map of tags to assign to the Internet Gateway"
+    type        = map(string)
+    default     = {}
+  }
+
  variable "public_subnet_cidrs" {
     description = "The CIDR block for the public subnet"
     type        = list(string)
     
    
  }
+ variable "public_subnet_tags" {
+    description = "A map of tags to assign to the public subnets"
+    type        = map(string)
+    default     = {}
+   
+ }
+
+ variable "private_subnet_cidrs" {
+    description = "The CIDR block for the private subnet"
+    type        = list(string)
+    
+ }
+    variable "private_subnet_tags" {
+        description = "A map of tags to assign to the private subnets"
+        type        = map(string)
+        default     = {}
+    
+    }
+
+
+variable "database_subnet_cidrs" {
+    description = "The CIDR block for the database subnet"
+    type        = list(string)
+  
+}
+
+
+variable "database_subnet_tags" {
+    description = "A map of tags to assign to the database subnets"
+    type        = map(string)
+    default     = {}
+  
+}
+
+variable "eip_tags" {
+    description = "A map of tags to assign to the NAT EIPs"
+    type        = map(string)
+    default     = {}
+  
+}
+
+variable "nat_gateway_tags" {
+    description = "A map of tags to assign to the NAT Gateways"
+    type        = map(string)
+    default     = {}
+  
+}
+
+variable "public_route_table_tags" {
+    description = "A map of tags to assign to the public route tables"
+    type        = map(string)
+    default     = {}
+  
+
+}
+
+variable "private_route_table_tags" {
+    description = "A map of tags to assign to the private route tables"
+    type        = map(string)
+    default     = {}
+  
+}
+
+variable "database_route_table_tags" {
+    description = "A map of tags to assign to the database route tables"
+    type        = map(string)
+    default     = {}
+  
+}
+
+variable "is_peering_required" {
+    description = "Boolean to specify whether VPC peering is required"
+    type        = bool
+    default     = false
+  
+
+}
+
+
+variable "vpc_peering_tags" {
+    description = "A map of tags to assign to the VPC peering connection"
+    type        = map(string)
+    default     = {}
+  
+}
